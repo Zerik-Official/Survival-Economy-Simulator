c298076f8513f14c6f881b9e89f366b67364ff17
Zerik-Official <173982082+Zerik-Official@users.noreply.github.com>
2026-03-05 21:47:13 -0500

    feat:partial improvement of interface function

--- interfaz.py
+++ interfaz.py
@@ -4,40 +4,41 @@
 #descripcion: Interfaz juego "WELCOME TO THE GAWE SERVIVAL TO EXTREMA"  
 
 #===========================================================================
-
-import inicio
-
 from colorama import init, Fore, Style
-#INICIO DE COLORAMA
 
-print(Fore.BLUE + "WELCOME TO THE GAME SERVIVAL TO EXTREMA" + Style.RESET_ALL)
+def color_off_resource(cuantity: int) -> str:
+    """
+    Determine the color for a resource based on its quantity.
 
-#TABLA DE RECURSO 
+    Args:
+        cuantity (int): The quantity of the resource.
 
-print(Fore.RED + "firewood" + Style.RESET_ALL)
-print(Fore.RED + "wheat" + Style.RESET_ALL)
-print(Fore.RED + "gold" + Style.RESET_ALL)
+    Returns:
+        str: The color code for the resource.
+    """
+    if cuantity < 20:
+        return Fore.RED
 
-
-print(Fore.GREEN + "firewood" + Style.RESET_ALL)
-print(Fore.GREEN + "wheat" + Style.RESET_ALL)
-print(Fore.GREEN + "gold" + Style.RESET_ALL)
-
-#FUNCIONES PARA DEFINIR COLOR 
-
-def color_recurso(valor: int) -> str:
-    if valor < 20:
-        return Fore.RED + str(valor)
-
-    elif valor > 60:
-        return Fore.YELLOW + str(valor)
+    elif cuantity > 60 and cuantity < 80:
+        return Fore.YELLOW
 
     else: 
-        return Fore.GREEN + str(valor)
+        return Fore.GREEN
 
-# funciones para mostrar recursos 
-def lista_recursos(resources:list[int, float]):
 
-    print("firewoo", resources[0])
-    print("wheat", resources[1])
-    print("gold", resources[2])
+def show_resources_list(player_name:str, current_day:int, current_day_text:str, resources:dict[str, int | float]) -> None:
+    """
+    Display the current resources and day information in a formatted manner.
+
+    Args:
+        current_day (int): The current day number.
+        current_day_text (str): The text representation of the current day.
+        resources (dict[str, int | float]): A dictionary containing the quantities of each resource.
+    Returns:
+        None
+    """
+
+    print("firewood", resources["firewood"])
+    print("wheat", resources["wheat"])
+    print("gold", resources["gold"])
+    print("poblacion", resources["population"])
