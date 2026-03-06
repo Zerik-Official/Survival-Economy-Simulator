f2a88edd17dcdd808a635ea3276b80a511bea282
Zerik-Official <173982082+Zerik-Official@users.noreply.github.com>
2026-03-05 21:46:09 -0500

    tests: initial tests of engine integration with the other modules

--- motor.py
+++ motor.py
@@ -9,18 +9,39 @@
 # --- Import files -----------------------------------------------
 # We import the necessary files for the game to function, such as the interface, events, consumption, and state.
 
-from .inicio import entry_difficulty
+import random
+from inicio import entry_difficulty, choose_difficulty
+from interfaz import color_off_resource, lista_recursos
+from eventos import apply_event
 
 
 # --- Day cycle -----------------------------------------------
 # We start the day cycle, iterating through the information of each day.
 
+day_of_weeks: list[str] = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 current_day: int = 1
 
-# Initializes the game engine. Reserved for future setup logic.
 
-def init_engine():
-    pass
+
+
+def init_engine() -> tuple[str, dict[str, int | float]]:
+    """
+    init_engine initializes the game engine by prompting the player to choose a difficulty level and setting up the initial resources based on that choice.
+    It returns the chosen difficulty and the initial resources for the game.
+    Args:
+        None
+    Returns:
+        difficulty (str): The chosen difficulty level as a string ("1", "2", or "3").
+        resources (dict[str, int | float]): A dictionary containing the initial resources for the game.
+    """
+    difficulty = entry_difficulty()
+
+    resources = choose_difficulty(difficulty)
+
+    randon_entry_day: int = random.randint(1, len(day_of_weeks) - 1)
+    current_day_text: str = day_of_weeks[randon_entry_day]
+
+    return difficulty, resources, current_day_text
 
 # Main game loop. Iterates through each day (1 to 10) while the game remains active.
 # On each day, it waits for player input, updates the day, and reloads all game modules.
@@ -31,4 +52,4 @@ def day_cycle():
 # --- Execute function in main -----------------------------------------------
 # Entry point: only runs if this file is executed directly, not when imported.
 if __name__ == "__main__":
-    pass
+    print(init_engine())
